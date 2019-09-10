85bd7d853e3774b6910bcb816a700df9f7e708cd
crain9412 <jwc9412@gmail.com>
2019-09-10 00:19:28 -0400

    Make build-and-deploy more reusable

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 #Build the local app and deploy it to minikube
-kubectl delete service hello-node
-kubectl delete deployment hello-node
+
+APP_NAME="hello-node"
+MINIKUBE_STATUS=$(minikube status | grep "host\: Running")
+SERVICE_STATUS=$(kubectl get services | grep "${APP_NAME}")
+DEPLOYMENT_STATUS=$(kubectl get deployments | grep "${APP_NAME}")
+
+if [ -z "${MINIKUBE_STATUS}" ]; then
+	minikube start
+fi
+
+if [ "${SERVICE_STATUS}" ]; then
+	kubectl delete service ${APP_NAME}
+fi
+
+if [ "${DEPLOYMENT_STATUS}" ]; then
+	kubectl delete deployment ${APP_NAME}
+fi
+
 eval $(minikube docker-env)
-docker build -t hello-node:0.0.1 .
-kubectl create deployment hello-node --image=hello-node:0.0.1
-kubectl expose deployment hello-node --type=LoadBalancer --port=8080 
-minikube service hello-node
+docker build -t ${APP_NAME}:0.0.1 .
+kubectl create deployment ${APP_NAME} --image=${APP_NAME}:0.0.1
+kubectl expose deployment ${APP_NAME} --type=LoadBalancer --port=8080 
+minikube service ${APP_NAME}
